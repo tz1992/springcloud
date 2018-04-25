ef54b098f4509fb77fcc51e78ff6728fbed5bd2e
Gogs <gogs@fake.local>
2018-04-25 17:22:05 +0800

    配置中心和单独服务

---
+++ myService/src/main/java/com/tz/myService/Application.java
@@ -0,0 +1,14 @@
+package com.tz.myService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class Application {
+public static void main(String[] args) {
+	new SpringApplication(Application.class).run(args);
+}
+}
