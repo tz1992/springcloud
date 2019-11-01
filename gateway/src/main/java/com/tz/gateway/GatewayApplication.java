7690f0d4ef853b8b1cfe68225fcbbc686b82e192
tz <616148442@qq.com>
2019-11-01 10:16:53 +0800

    fix

---
+++ gateway/src/main/java/com/tz/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.tz.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class GatewayApplication {
+  
+ 
+  
+  
+public static void main(String[] args) {
+  
+	new SpringApplication(GatewayApplication.class).run(args);
+	
+}
+}
